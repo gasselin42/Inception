e3d3e303563cef326d951b5d323d0a0893a4773d
Gabriel Asselin <gasselin@c2r2p8.42quebec.com>
2022-07-07 11:46:08 -0400

    5th push

--- srcs/requirements/mariadb/tools/mariadb_script.sh
+++ srcs/requirements/mariadb/tools/mariadb_script.sh
@@ -2,22 +2,18 @@
 
 set -euxo pipefail
 
-if [ "$1" = "mysqld"]; then
+if [ ! -d "/run/mysqld" ]; then
+	mkdir -p /run/mysqld
+fi
 
-	if [ ! -d "/run/mysqld" ]; then
-		mkdir -p /run/mysqld
-	fi
+if [ ! -d "/var/lib/mysql/mysql" ]; then
+	mysql_install_db --basedir=/usr --datadir=/var/lib/mysql --user=root --skip-test-db --rpm > /dev/null
+fi
 
-	if [ ! -d "/var/lib/mysql/mysql" ]; then
-		mysql_install_db --basedir=/usr --datadir=/var/lib/mysql --user=root --skip-test-db --rpm > /dev/null
-	fi
-
-	if [ -d $DB_PATH/$DB_NAME ]; then
-		echo "$DB_NAME ALREADY EXISTS"
-	else
-		mysqld --user=root --bootstrap < ./conf.sql
-	fi
-
+if [ -d $DB_PATH/$DB_NAME ]; then
+	echo "$DB_NAME ALREADY EXISTS"
+else
+	mysqld --user=root --bootstrap < ./conf.sql
 fi
 
 exec "$@"
