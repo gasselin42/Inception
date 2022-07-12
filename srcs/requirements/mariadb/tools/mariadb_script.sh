a906d65c804fb64b40dad8495799ae8b6263ed1a
Gabriel Asselin <gasselin@c2r3p6.42quebec.com>
2022-07-12 14:56:14 -0400

    11th push

--- srcs/requirements/mariadb/tools/mariadb_script.sh
+++ srcs/requirements/mariadb/tools/mariadb_script.sh
@@ -7,10 +7,12 @@ sed -Ei '/^(#)?port/c port = 3306' /etc/mysql/mariadb.conf.d/50-server.cnf
 sed -Ei "/^datadir/c datadir = /var/lib/mysql" /etc/mysql/mariadb.conf.d/50-server.cnf
 
 if [ ! -d "/run/mysqld" ]; then
-	chown -R mysql:mysql /run/mysqld
+	mkdir -p /run/mysqld
+	# chown -R mysql:mysql /run/mysqld
 fi
 
 if [ ! -d "/var/lib/mysql/mysql" ]; then
+	# chown -R mysql:mysql /var/lib/mysql
 	mysql_install_db --basedir=/usr --datadir=/var/lib/mysql --user=mysql --skip-test-db --rpm > /dev/null
 fi
 
