f1cd9b038c2a096150d54eb67d0edaf4edddb50d
Gabriel Asselin <gasselin@c2r3p6.42quebec.com>
2022-07-12 15:00:23 -0400

    11th push

--- srcs/requirements/mariadb/tools/mariadb_script.sh
+++ srcs/requirements/mariadb/tools/mariadb_script.sh
@@ -6,20 +6,14 @@ sed -i 's/.*bind-address.*/bind-address = 0.0.0.0/' /etc/mysql/mariadb.conf.d/50
 sed -Ei '/^(#)?port/c port = 3306' /etc/mysql/mariadb.conf.d/50-server.cnf
 sed -Ei "/^datadir/c datadir = /var/lib/mysql" /etc/mysql/mariadb.conf.d/50-server.cnf
 
-if [ ! -d "/run/mysqld" ]; then
-	mkdir -p /run/mysqld
-	# chown -R mysql:mysql /run/mysqld
-fi
-
 if [ ! -d "/var/lib/mysql/mysql" ]; then
-	# chown -R mysql:mysql /var/lib/mysql
-	mysql_install_db --basedir=/usr --datadir=/var/lib/mysql --user=mysql --skip-test-db --rpm > /dev/null
+	mysql_install_db --basedir=/usr --datadir=/var/lib/mysql --user=root --skip-test-db --rpm > /dev/null
 fi
 
 if [ -d $DB_PATH/$DB_NAME ]; then
 	echo "$DB_NAME ALREADY EXISTS"
 else
-	mysqld --user=mysql --bootstrap < ./conf.sql
+	mysqld --user=root --bootstrap < ./conf.sql
 fi
 
 exec "$@"
