f4a88af85cc7660671c72f02ae25d5a201995e19
Gabriel Asselin <gasselin@c1r6p5.42quebec.com>
2022-09-01 15:01:41 -0400

    Debugging WP

--- srcs/requirements/mariadb/tools/entry.sh
+++ srcs/requirements/mariadb/tools/entry.sh
@@ -4,58 +4,32 @@ set -euo pipefail
 
 if [ "$1" = "mysqld" ]; then
 
-	# dataDB=/var/lib/mysql/init_dataDB.sql
+	dataDB=/var/lib/mysql/init_dataDB.sql
 
-	# if [ ! -f $dataDB ]; then
+	if [ ! -f $dataDB ]; then
 		mysql_install_db --datadir=/var/lib/mysql --user=mysql --skip-test-db > /dev/null
 
-# 		cat > $dataDB <<EOF
-# CREATE DATABASE IF NOT EXISTS $DB_NAME;
-# CREATE USER IF NOT EXISTS '$DB_USR'@'%' IDENTIFIED BY '$DB_PWD';
-# GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$DB_USR'@'%';
-# ALTER USER 'root'@'localhost' IDENTIFIED BY '$DB_ROOT_PWD';
-# FLUSH PRIVILEGES;
-# EOF
+		cat > $dataDB <<EOF
+CREATE DATABASE IF NOT EXISTS $DB_NAME;
+CREATE USER IF NOT EXISTS '$DB_USR'@'%' IDENTIFIED BY '$DB_PWD';
+GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$DB_USR'@'%';
+ALTER USER 'root'@'localhost' IDENTIFIED BY '$DB_ROOT_PWD';
+FLUSH PRIVILEGES;
+EOF
 
-		mysqld --user=mysql --datadir=/var/lib/mysql &
+		for i in {0..30}; do
+			if mysqld --user=root --password=root --database=mysql <<<'SELECT 1;' &> /dev/null; then
+				break
+			fi
+			sleep 1
+		done
 
-		sleep 5
+		if [ "$i" = 30 ]; then
+			echo "Cannot connect to databse"
+		fi
 
-		mysql -e "CREATE DATABASE IF NOT EXISTS ${DB_NAME};"
-
-		echo "CREATED DATABASE"
-
-		mysql -e "CREATE USER IF NOT EXISTS '${DB_USR}'@'localhost' IDENTIFIED BY '${DB_PWD}';"
-
-		echo "CREATED USER"
-
-		mysql -e "GRANT ALL PRIVILEGES ON \`${DB_NAME}\`.* TO \`${DB_USR}\`@'%' IDENTIFIED BY '${DB_PWD}';"
-
-		echo "PRIVILEGES GRANTED"
-
-		mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${DB_ROOT_PWD}';"
-
-		echo "ROOT PASSWORD SET"
-
-		mysql -u root -p${DB_ROOT_PWD} -e "FLUSH PRIVILEGES;"
-
-		echo "PRIVILEGES FLUSHED"
-
-		killall mysqld
-
-		# for i in {0..30}; do
-		# 	if mysql --user=root --password=root --database=mysql <<<'SELECT 1;' &> /dev/null; then
-		# 		break
-		# 	fi
-		# 	sleep 1
-		# done
-
-		# if [ "$i" = 30 ]; then
-		# 	echo "Cannot connect to databse"
-		# fi
-
-		# mysql --user=root --password=root < $dataDB && killall mysqld
-	# fi
+		mysqld --user=root --password=root < $dataDB && killall mysqld
+	fi
 
 fi
 
