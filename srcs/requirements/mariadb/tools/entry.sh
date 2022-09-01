3e025641c3e73eb615e1172f3dd2ae985a520833
Gabriel Asselin <gasselin@c1r6p5.42quebec.com>
2022-09-01 14:31:08 -0400

    Debugging WP

--- srcs/requirements/mariadb/tools/entry.sh
+++ srcs/requirements/mariadb/tools/entry.sh
@@ -11,39 +11,39 @@ if [ "$1" = "mysqld" ]; then
 
 		mysql_install_db --datadir=/var/lib/mysql --user=mysql --skip-test-db > /dev/null
 
-# 		cat > $dataDB <<EOF
-# CREATE DATABASE IF NOT EXISTS $DB_NAME;
-# CREATE USER IF NOT EXISTS '$DB_USR'@'%' IDENTIFIED BY '$DB_PWD';
-# GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$DB_USR'@'%';
-# ALTER USER 'root'@'localhost' IDENTIFIED BY '$DB_ROOT_PWD';
-# FLUSH PRIVILEGES;
-# EOF
+		cat > $dataDB <<EOF
+CREATE DATABASE IF NOT EXISTS $DB_NAME;
+CREATE USER IF NOT EXISTS '$DB_USR'@'%' IDENTIFIED BY '$DB_PWD';
+GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$DB_USR'@'%';
+ALTER USER 'root'@'localhost' IDENTIFIED BY '$DB_ROOT_PWD';
+FLUSH PRIVILEGES;
+EOF
 
 		mysqld --user=mysql --datadir=/var/lib/mysql &
 
 		sleep 5
 
-		mysql -e "CREATE DATABASE IF NOT EXISTS ${DB_NAME};"
+		# mysql -e "CREATE DATABASE IF NOT EXISTS ${DB_NAME};"
 
-		echo "CREATED DATABASE"
+		# echo "CREATED DATABASE"
 
-		mysql -e "CREATE USER IF NOT EXISTS '${DB_USR}'@'localhost' IDENTIFIED BY '${DB_PWD}';"
+		# mysql -e "CREATE USER IF NOT EXISTS '${DB_USR}'@'localhost' IDENTIFIED BY '${DB_PWD}';"
 
-		echo "CREATED USER"
+		# echo "CREATED USER"
 
-		mysql -e "GRANT ALL PRIVILEGES ON \`${DB_NAME}\`.* TO \`${DB_USR}\`@'%' IDENTIFIED BY '${DB_PWD}';"
+		# mysql -e "GRANT ALL PRIVILEGES ON \`${DB_NAME}\`.* TO \`${DB_USR}\`@'%' IDENTIFIED BY '${DB_PWD}';"
 
-		echo "PRIVILEGES GRANTED"
+		# echo "PRIVILEGES GRANTED"
 
-		mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${DB_ROOT_PWD}';"
+		# mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${DB_ROOT_PWD}';"
 
-		echo "ROOT PASSWORD SET"
+		# echo "ROOT PASSWORD SET"
 
-		mysql -u root -p${DB_ROOT_PWD} -e "FLUSH PRIVILEGES;"
+		# mysql -u root -p${DB_ROOT_PWD} -e "FLUSH PRIVILEGES;"
 
-		echo "PRIVILEGES FLUSHED"
+		# echo "PRIVILEGES FLUSHED"
 
-		killall mysqld
+		# killall mysqld
 
 		# for i in {0..30}; do
 		# 	if mysql --user=root --password=root --database=mysql <<<'SELECT 1;' &> /dev/null; then
@@ -56,7 +56,7 @@ if [ "$1" = "mysqld" ]; then
 		# 	echo "Cannot connect to databse"
 		# fi
 
-		# mysql --user=root --password=root < $dataDB && killall mysqld
+		mysql --user=root --password=root < $dataDB && killall mysqld
 	fi
 
 fi
