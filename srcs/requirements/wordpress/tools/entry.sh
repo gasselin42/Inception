9e7f9e1bbdd0edf5ce9aea7ae8f738687e022b49
Gabriel Asselin <gasselin@c1r6p5.42quebec.com>
2022-09-01 15:24:49 -0400

    Debugging WP

--- srcs/requirements/wordpress/tools/entry.sh
+++ srcs/requirements/wordpress/tools/entry.sh
@@ -2,55 +2,51 @@
 
 set -euo pipefail
 
-if [ "$1" = "php-fpm7.3" ]; then
-
-	for i in {0..30}; do
-		if mariadb -h$DB_HOST -u$DB_USR -p$DB_PWD --database=$DB_NAME <<< 'SELECT 1;' &>/dev/null; then
-			break
-		fi
-		sleep 1
-	done
-
-	if [ "$i" = 30 ]; then
-		echo "Can't connect to database"
+for i in {0..30}; do
+	if mariadb -h$DB_HOST -u$DB_USR -p$DB_PWD --database=$DB_NAME <<< 'SELECT 1;' &>/dev/null; then
+		break
 	fi
+	sleep 1
+done
 
-	if [ ! -f "/var/www/html/wp-config.php" ]; then
+if [ "$i" = 30 ]; then
+	echo "Can't connect to database"
+fi
 
-		wp core download --allow-root --path="/var/www/html"
-		rm -f /var/www/html/wp-config-sample.php
+if [ ! -f "/var/www/html/wp-config.php" ]; then
 
-		wp config create \
-			--allow-root \
-			--dbname=$DB_NAME \
-			--dbuser=$DB_USR \
-			--dbpass=$DB_PWD \
-			--dbhost=$DB_HOST \
-			--dbcharset="utf8" \
-			--dbcollate="utf8_general_ci" \
-			--path="/var/www/html"
+	wp core download --allow-root --path="/var/www/html"
+	rm -f /var/www/html/wp-config-sample.php
 
-		wp core install \
-			--allow-root \
-			--title="${WP_TITLE}" \
-			--admin_user="${WP_ADMIN_LOGIN}" \
-			--admin_password="${WP_ADMIN_PWD}" \
-			--admin_email="${WP_ADMIN_EMAIL}" \
-			--skip-email \
-			--url="${DOMAIN_NAME}" \
-			--path="/var/www/html"
+	wp config create \
+		--allow-root \
+		--dbname=$DB_NAME \
+		--dbuser=$DB_USR \
+		--dbpass=$DB_PWD \
+		--dbhost=$DB_HOST \
+		--dbcharset="utf8" \
+		--dbcollate="utf8_general_ci" \
+		--path="/var/www/html"
 
-		wp user create \
-			--allow-root \
-			$WP_USER_LOGIN \
-			$WP_USER_EMAIL \
-			--role=author \
-			--user_pass=$WP_USER_PWD \
-			--path="/var/www/html"
+	wp core install \
+		--allow-root \
+		--title="${WP_TITLE}" \
+		--admin_user="${WP_ADMIN_LOGIN}" \
+		--admin_password="${WP_ADMIN_PWD}" \
+		--admin_email="${WP_ADMIN_EMAIL}" \
+		--skip-email \
+		--url="${DOMAIN_NAME}" \
+		--path="/var/www/html"
 
-		wp theme install twentyten --allow-root --activate --path="/var/www/html"
+	wp user create \
+		--allow-root \
+		$WP_USER_LOGIN \
+		$WP_USER_EMAIL \
+		--role=author \
+		--user_pass=$WP_USER_PWD \
+		--path="/var/www/html"
 
-	fi
+	wp theme install twentyten --allow-root --activate --path="/var/www/html"
 
 fi
 
