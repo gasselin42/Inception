9d9162e37e509c2c67680a4db397ecb1dbda7fac
Gabriel Asselin <gasselin@c1r6p5.42quebec.com>
2022-09-01 14:27:24 -0400

    Debugging WP

--- srcs/requirements/mariadb/tools/entry.sh
+++ srcs/requirements/mariadb/tools/entry.sh
@@ -2,35 +2,63 @@
 
 set -euo pipefail
 
-dataDB=/var/lib/mysql/init_dataDB.sql
+if [ "$1" = "mysqld" ]; then
 
-if [ ! -f $dataDB ]; then
-	chown -R mysql:mysql /var/lib/mysql
+	# dataDB=/var/lib/mysql/init_dataDB.sql
 
-	mysql_install_db --datadir=/var/lib/mysql --user=mysql --skip-test-db > /dev/null
+	if [ ! -f $dataDB ]; then
+		chown -R mysql:mysql /var/lib/mysql
 
-	cat > $dataDB <<EOF
-CREATE DATABASE IF NOT EXISTS $DB_NAME;
-CREATE USER IF NOT EXISTS '$DB_USR'@'%' IDENTIFIED BY '$DB_PWD';
-GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$DB_USR'@'%';
-ALTER USER 'root'@'localhost' IDENTIFIED BY '$DB_ROOT_PWD';
-FLUSH PRIVILEGES;
-EOF
+		mysql_install_db --datadir=/var/lib/mysql --user=mysql --skip-test-db > /dev/null
 
-	mysqld --user=mysql --datadir=/var/lib/mysql &
+# 		cat > $dataDB <<EOF
+# CREATE DATABASE IF NOT EXISTS $DB_NAME;
+# CREATE USER IF NOT EXISTS '$DB_USR'@'%' IDENTIFIED BY '$DB_PWD';
+# GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$DB_USR'@'%';
+# ALTER USER 'root'@'localhost' IDENTIFIED BY '$DB_ROOT_PWD';
+# FLUSH PRIVILEGES;
+# EOF
 
-	for i in {0..30}; do
-		if mysql --user=root --password=root --database=mysql <<<'SELECT 1;' &> /dev/null; then
-			break
-		fi
-		sleep 1
-	done
+		mysqld --user=mysql --datadir=/var/lib/mysql &
 
-	if [ "$i" = 30 ]; then
-		echo "Cannot connect to databse"
+		sleep 5
+
+		mysql -e "CREATE DATABASE IF NOT EXISTS ${DB_NAME};"
+
+		echo "CREATED DATABASE"
+
+		mysql -e "CREATE USER IF NOT EXISTS '${DB_USR}'@'localhost' IDENTIFIED BY '${DB_PWD}';"
+
+		echo "CREATED USER"
+
+		mysql -e "GRANT ALL PRIVILEGES ON \`${DB_NAME}\`.* TO \`${DB_USR}\`@'%' IDENTIFIED BY '${DB_PWD}';"
+
+		echo "PRIVILEGES GRANTED"
+
+		mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${DB_ROOT_PWD}';"
+
+		echo "ROOT PASSWORD SET"
+
+		mysql -u root -p${DB_ROOT_PWD} -e "FLUSH PRIVILEGES;"
+
+		echo "PRIVILEGES FLUSHED"
+
+		killall mysqld
+
+		# for i in {0..30}; do
+		# 	if mysql --user=root --password=root --database=mysql <<<'SELECT 1;' &> /dev/null; then
+		# 		break
+		# 	fi
+		# 	sleep 1
+		# done
+
+		# if [ "$i" = 30 ]; then
+		# 	echo "Cannot connect to databse"
+		# fi
+
+		# mysql --user=root --password=root < $dataDB && killall mysqld
 	fi
 
-	mysql --user=root --password=root < $dataDB && killall mysqld
 fi
 
 exec "$@"
